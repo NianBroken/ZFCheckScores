ae03af5a50830f740c97d34c03e9f0a4ce884eec
NianBroken <suinian666@gmail.com>
2024-07-15 15:38:41 +0800

    当检测到验证码时，强制结束程序。

--- scripts/user_login.py
+++ scripts/user_login.py
@@ -25,6 +25,8 @@ def login(url, username, password):
     if cookies == {}:
         lgn = student_client.login(username, password)
         if lgn["code"] == 1001:
+            sys.exit("你的教务系统登录需要验证码，因此你无法使用此项目。")
+            """
             verify_data = lgn["data"]
             with open(os.path.abspath("kaptcha.png"), "wb") as pic:
                 pic.write(base64.b64decode(verify_data.pop("kaptcha_pic")))
@@ -34,6 +36,7 @@ def login(url, username, password):
                 pprint(ret)
                 sys.exit()
             pprint(ret)
+            """
         elif lgn["code"] != 1000:
             pprint(lgn)
             sys.exit()
